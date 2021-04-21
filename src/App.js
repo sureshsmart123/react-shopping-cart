bee14261916cd072addeb0d6167c6654a6034fd6
sureshsmart123 <sureshkumar.pacet@gmail.com>
2021-04-21 13:24:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product Details</main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
