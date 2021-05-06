0a1c4271066e9f562d6ee1decdf426239d2277b3
sureshsmart123 <sureshkumar.pacet@gmail.com>
2021-05-06 18:08:15 +0530

    add redux to Filter Component

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const shortid=require("shortid");
+const shortid = require("shortid");
 
-const app=express();
+const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/react-shopping-cart-db",{
-useNewUrlParser: true,
-useCreateIndex: true,
-useUnifiedTopology: true,
+mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
-const Product=mongoose.model("products",new mongoose.Schema({
-    _id:{type: String, default: shortid.generate},
+const Product = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
     title: String,
     description: String,
     image: String,
-    price: String,
+    price: Number,
     availableSizes: [String],
-})
+  })
 );
-app.get("/api/products",async(req,res)=>{
-const products=await Product.find({});
-res.send(products);
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
 });
 
-app.post("/api/products",async(req,res)=>{
-    const newProduct=new Product(req.body);
-    const saveProduct=await newProduct.save();
-    res.send(saveProduct);
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
 });
 
-app.delete("/api/products/:id",async(req,res)=>{
-    const deletedProduct=await Product.findByIdAndDelete(req.params.id);
-    res.send(deletedProduct);
-})
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
 
-const port=process.env.PORT || 5000;
-app.listen(port,()=>console.log("serve at http://localhost:5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at http://localhost:5000"));
